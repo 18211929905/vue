d05ec80feed2db0987dd8e618e98dda1d103d71c
lili <10523907+lilijiak@user.noreply.gitee.com>
2026-06-22 09:59:16 +0800

    提交git文件 最初始化vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>Vue 2 基础项目已经可运行。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Hello Vue 2",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  margin-top: 60px;
+  text-align: center;
+  color: #2c3e50;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  margin-bottom: 12px;
+}
+</style>
